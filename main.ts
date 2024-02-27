037efdce22ed114c2ba1199215b88790f3e54414
rey07za <156244750+rey07za@users.noreply.github.com>
2024-02-27 14:14:26 -0600

    Fixed function functionality, list works now.

--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
 namespace SpriteKind {
     export const powerUpSprite = SpriteKind.create()
     export const BetterPowerUp = SpriteKind.create()
+    export const secondEnemy = SpriteKind.create()
+    export const thirdEnemy = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.BetterPowerUp, function (sprite, otherSprite) {
+    playerScore = playerScore + 3
+    info.changeScoreBy(3)
+    sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.powerUpSprite, function (sprite, otherSprite) {
+    playerScore = playerScore + 2
+    info.changeScoreBy(2)
+    sprites.destroy(otherSprite)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.secondEnemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-3)
+})
 info.onLifeZero(function () {
     game.gameOver(true)
     game.reset()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.thirdEnemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-2)
 })
 function EnemyDifficultySpawner (gameModeDifficulty: number) {
     if (gameModeDifficulty < 5) {
@@ -60,7 +80,15 @@ function EnemyDifficultySpawner (gameModeDifficulty: number) {
             . f b b b b b b b b c f . . . . 
             . . f b b b b b b c f . . . . . 
             . . . f f f f f f f . . . . . . 
-            `, SpriteKind.Enemy)
+            `, SpriteKind.secondEnemy)
+        alienEnemy.setPosition(151, randint(0, 160))
+        alienEnemy.vx = -50
+        for (let index = 0; index < 4; index++) {
+            powerUpsList.shift()
+        }
+        powerUp = sprites.create(powerUpsList._pickRandom(), SpriteKind.powerUpSprite)
+        powerUp.vx = -30
+        powerUp.setPosition(151, randint(0, 160))
     } else {
         alienEnemy = sprites.create(img`
             ..............ccccccccc........
@@ -93,18 +121,28 @@ function EnemyDifficultySpawner (gameModeDifficulty: number) {
             .........cc555555bc............
             .........cc55555555c...........
             ..........cccccccccc...........
-            `, SpriteKind.Enemy)
+            `, SpriteKind.thirdEnemy)
+        alienEnemy.setPosition(151, randint(0, 160))
+        alienEnemy.vx = -35
+        for (let index = 0; index < 2; index++) {
+            powerUpsList.pop()
+        }
+        for (let index = 0; index < 2; index++) {
+            powerUpsList.shift()
+        }
+        morePowerUp = sprites.create(powerUpsList._pickRandom(), SpriteKind.BetterPowerUp)
+        morePowerUp.vx = -30
+        morePowerUp.setPosition(151, randint(0, 160))
     }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    playerScore = playerScore + 2
-    info.changeScoreBy(2)
     sprites.destroy(otherSprite)
     info.changeLifeBy(-1)
 })
+let morePowerUp: Sprite = null
 let powerUp: Sprite = null
-let alienEnemy: Sprite = null
 let powerUpsList: Image[] = []
+let alienEnemy: Sprite = null
 let playerScore = 0
 game.setGameOverScoringType(game.ScoringType.HighScore)
 scene.setBackgroundImage(img`
@@ -251,120 +289,121 @@ let gameModeDifficulty = game.askForNumber("Gamemode Difficulty?", 2)
 while (gameModeDifficulty > 10) {
     gameModeDifficulty = game.askForNumber("Gamemode Difficulty?", 2)
 }
+playerSprite.setStayInScreen(true)
 controller.moveSprite(playerSprite, 100, 100)
 playerScore = 0
 info.setScore(0)
 info.setLife(6)
-powerUpsList = [
-img`
-    . . . . . . . 6 . . . . . . . . 
-    . . . . . . 8 6 6 . . . 6 8 . . 
-    . . . e e e 8 8 6 6 . 6 7 8 . . 
-    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-    e e 2 e 2 2 4 2 2 e e e c . . . 
-    e e e e 2 e 2 2 e e e c . . . . 
-    e e e 2 e e c e c c c . . . . . 
-    . c c c c c c c . . . . . . . . 
-    `,
-img`
-    . . . . . 3 3 b 3 3 d d 3 3 . . 
-    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
-    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
-    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
-    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
-    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
-    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
-    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
-    . 4 4 d 1 1 1 1 1 1 d d d b b . 
-    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
-    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
-    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
-    4 5 5 d 5 5 d b b b d d 3 . . . 
-    4 5 5 5 d d d d 4 4 b 3 . . . . 
-    . 4 5 5 5 4 4 4 . . . . . . . . 
-    . . 4 4 4 . . . . . . . . . . . 
-    `,
-img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
-    `,
-img`
-    4 4 4 . . 4 4 4 4 4 . . . . . . 
-    4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
-    b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
-    . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
-    . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
-    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
-    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
-    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
-    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
-    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
-    . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
-    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
-    . . c 4 4 d 4 4 4 4 4 d d 5 d c 
-    . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
-    . . . . c c b 4 4 4 b b 4 5 4 4 
-    . . . . . . c c c c c c b b 4 . 
-    `,
-img`
-    . . . . c c c b b b b b . . . . 
-    . . c c b 4 4 4 4 4 4 b b b . . 
-    . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-    . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-    e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-    e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-    e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-    . e b 4 4 4 4 4 5 4 4 4 4 b e . 
-    8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-    8 7 2 e e e e e e e e e e 2 7 8 
-    e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-    e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-    e b e 8 8 c c 8 8 c c c 8 e b e 
-    e e b e c c e e e e e c e b e e 
-    . e e b b 4 4 4 4 4 4 4 4 e e . 
-    . . . c c c c c e e e e e . . . 
-    `,
-img`
-    . . 2 2 b b b b b . . . . . . . 
-    . 2 b 4 4 4 4 4 4 b . . . . . . 
-    2 2 4 4 4 4 d d 4 4 b . . . . . 
-    2 b 4 4 4 4 4 4 d 4 b . . . . . 
-    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
-    2 2 b 4 4 4 4 4 4 4 b e . . . . 
-    . 2 b b b 4 4 4 b b b e . . . . 
-    . . e b b b b b b b e e . . . . 
-    . . . e e b 4 4 b e e e b . . . 
-    . . . . . e e e e e e b d b b . 
-    . . . . . . . . . . . b 1 1 1 b 
-    . . . . . . . . . . . c 1 d d b 
-    . . . . . . . . . . . c 1 b c . 
-    . . . . . . . . . . . . c c . . 
-    `
-]
 game.onUpdateInterval(1500, function () {
+    powerUpsList = [
+    img`
+        . . . . . . . 6 . . . . . . . . 
+        . . . . . . 8 6 6 . . . 6 8 . . 
+        . . . e e e 8 8 6 6 . 6 7 8 . . 
+        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+        e e 2 e 2 2 4 2 2 e e e c . . . 
+        e e e e 2 e 2 2 e e e c . . . . 
+        e e e 2 e e c e c c c . . . . . 
+        . c c c c c c c . . . . . . . . 
+        `,
+    img`
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+        . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+        . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+        . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+        . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+        . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+        . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+        . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+        . 4 4 d 1 1 1 1 1 1 d d d b b . 
+        . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+        4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+        4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `,
+    img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `,
+    img`
+        4 4 4 . . 4 4 4 4 4 . . . . . . 
+        4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+        b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+        . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+        . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+        b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+        c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+        c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
+        c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
+        c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
+        . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
+        . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
+        . . c 4 4 d 4 4 4 4 4 d d 5 d c 
+        . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
+        . . . . c c b 4 4 4 b b 4 5 4 4 
+        . . . . . . c c c c c c b b 4 . 
+        `,
+    img`
+        . . . . c c c b b b b b . . . . 
+        . . c c b 4 4 4 4 4 4 b b b . . 
+        . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+        . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+        e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+        e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+        e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+        . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+        8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+        8 7 2 e e e e e e e e e e 2 7 8 
+        e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+        e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+        e b e 8 8 c c 8 8 c c c 8 e b e 
+        e e b e c c e e e e e c e b e e 
+        . e e b b 4 4 4 4 4 4 4 4 e e . 
+        . . . c c c c c e e e e e . . . 
+        `,
+    img`
+        . . 2 2 b b b b b . . . . . . . 
+        . 2 b 4 4 4 4 4 4 b . . . . . . 
+        2 2 4 4 4 4 d d 4 4 b . . . . . 
+        2 b 4 4 4 4 4 4 d 4 b . . . . . 
+        2 b 4 4 4 4 4 4 4 d 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 b . . . . 
+        2 b 4 4 4 4 4 4 4 4 4 e . . . . 
+        2 2 b 4 4 4 4 4 4 4 b e . . . . 
+        . 2 b b b 4 4 4 b b b e . . . . 
+        . . e b b b b b b b e e . . . . 
+        . . . e e b 4 4 b e e e b . . . 
+        . . . . . e e e e e e b d b b . 
+        . . . . . . . . . . . b 1 1 1 b 
+        . . . . . . . . . . . c 1 d d b 
+        . . . . . . . . . . . c 1 b c . 
+        . . . . . . . . . . . . c c . . 
+        `
+    ]
     EnemyDifficultySpawner(gameModeDifficulty)
 })
