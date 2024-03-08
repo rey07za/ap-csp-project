270ed0ad014e06acc6a98ad8c13234b9eba166d9
rey07za <156244750+rey07za@users.noreply.github.com>
2024-03-08 14:51:47 -0600

    one step away from final

--- main.ts
+++ main.ts
@@ -5,13 +5,11 @@ namespace SpriteKind {
     export const thirdEnemy = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.BetterPowerUp, function (sprite, otherSprite) {
-    playerScore = playerScore + 3
     info.changeScoreBy(3)
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
     sprites.destroy(otherSprite)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.powerUpSprite, function (sprite, otherSprite) {
-    playerScore = playerScore + 2
     info.changeScoreBy(2)
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
     sprites.destroy(otherSprite)
@@ -55,16 +53,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.thirdEnemy, function (sprite, ot
 })
 function EnemyDifficultySpawner (gameModeDifficulty: number) {
     if (gameModeDifficulty < 5) {
-        alienEnemy = sprites.create(assets.image`myImage`, SpriteKind.Enemy)
-        alienEnemy.setPosition(151, randint(20, 135))
-        alienEnemy.vx = -20
-        for (let index = 0; index < 4; index++) {
-            powerUpsList.pop()
-        }
-        powerUp = sprites.create(powerUpsList._pickRandom(), SpriteKind.powerUpSprite)
-        powerUp.vx = -30
-        powerUp.setPosition(151, randint(20, 135))
-    } else if (gameModeDifficulty > 5) {
         alienEnemy = sprites.create(img`
             . a a a . . . . . . . . . . . . 
             b 1 1 1 b . . . . . . . a b a . 
@@ -82,6 +70,33 @@ function EnemyDifficultySpawner (gameModeDifficulty: number) {
             . . . . a a a b a a . . . . . . 
             . . . . b a a a a a . . . . . . 
             . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        alienEnemy.setPosition(151, randint(20, 135))
+        alienEnemy.vx = -20
+        for (let index = 0; index < 4; index++) {
+            powerUpsList.pop()
+        }
+        powerUp = sprites.create(powerUpsList._pickRandom(), SpriteKind.powerUpSprite)
+        powerUp.vx = -30
+        powerUp.setPosition(151, randint(20, 135))
+    } else if (gameModeDifficulty > 5) {
+        alienEnemy = sprites.create(img`
+            . . 2 2 2 2 2 4 4 4 4 4 4 4 . . 
+            . 2 2 4 4 2 2 2 2 2 2 2 2 2 2 . 
+            . 2 2 4 f 2 2 2 2 2 2 f 2 2 2 4 
+            . 2 2 2 2 f 2 2 2 2 f 2 2 2 2 4 
+            . 2 2 2 2 2 f 2 2 f 2 4 2 2 2 4 
+            . 2 2 2 2 2 5 2 2 5 2 4 2 2 2 4 
+            . 2 2 2 2 2 f 2 2 f 2 2 4 2 2 4 
+            . 2 4 2 2 2 2 2 2 2 2 2 2 2 2 4 
+            . 2 2 4 2 2 2 f f 2 2 2 2 2 2 4 
+            . . 2 4 4 2 2 f f 2 2 2 2 2 4 4 
+            . . . 2 2 2 2 2 2 2 4 4 4 4 4 . 
+            . . 4 2 2 . 2 . . 2 . . 4 4 2 2 
+            . 4 2 . 2 4 2 2 . 2 4 . 2 2 . 2 
+            . 2 . . . 2 . 2 . . 4 . . 2 . . 
+            . . 2 . 2 2 . 2 . 2 2 . . 4 2 . 
+            . . 2 . 2 . . 2 . 2 . . . . 2 . 
             `, SpriteKind.secondEnemy)
         alienEnemy.setPosition(151, randint(20, 135))
         alienEnemy.vx = -50
@@ -92,38 +107,7 @@ function EnemyDifficultySpawner (gameModeDifficulty: number) {
         powerUp.vx = -30
         powerUp.setPosition(151, randint(20, 135))
     } else {
-        alienEnemy = sprites.create(img`
-            ..............ccccccccc........
-            ............cc555555555cc......
-            ...........c5555555555555c.....
-            ..........c55555555555555dc....
-            .........c555555555555b5bdc....
-            .........555bc1555555555bdcccc.
-            ........c555ccc55555555bbdccddc
-            ........c555bcb5555555ccddcdddc
-            .......c555555555551ccccddbdddc
-            .......c555555b555c1cccbddbbdbc
-            .......c5555555bbc33333ddddbcc.
-            .......c555555555bc333555ddbc..
-            .......c5555555555555555555c...
-            .......cd555555555555cccc555c..
-            .......cd55555555555c555c555c..
-            .......cdd555555555b5555b555c..
-            .......cddd55555ddbb555bb555c..
-            .......cdddd55555555555b5555c..
-            .......cddddd5555555ddb5555dc..
-            c......cdddddd555555555555dcc..
-            cc...ccddddddd555555555555dc...
-            cdccccdddddd555555d55555ddcc...
-            cdddddddddbd5555555ddddddccccc.
-            ccdddddddbb55555555bddddccbddc.
-            .ccddddddbd55555555bdddccdddc..
-            ..cccddddbd5555555cddcccddbc...
-            ....ccccccd555555bcccc.cccc....
-            .........cc555555bc............
-            .........cc55555555c...........
-            ..........cccccccccc...........
-            `, SpriteKind.thirdEnemy)
+        alienEnemy = sprites.create(assets.image`myImage`, SpriteKind.thirdEnemy)
         alienEnemy.setPosition(151, randint(20, 135))
         alienEnemy.vx = -35
         for (let index = 0; index < 2; index++) {
@@ -156,9 +140,10 @@ let powerUp: Sprite = null
 let powerUpsList: Image[] = []
 let alienEnemy: Sprite = null
 let projectile: Sprite = null
-let playerScore = 0
 let playerSprite: Sprite = null
 let timeCheck = 0
+game.splash("Don't let the enemy aliens get past you!")
+game.splash("Use the space bar to shoot!")
 game.setGameOverScoringType(game.ScoringType.HighScore)
 timeCheck = game.runtime()
 scene.setBackgroundImage(img`
@@ -308,13 +293,15 @@ while (gameModeDifficulty > 10) {
 playerSprite.setStayInScreen(true)
 playerSprite.setPosition(6, 59)
 controller.moveSprite(playerSprite, 0, 100)
-playerScore = 0
+let playerScore = 0
 info.setScore(0)
 info.setLife(25)
 game.onUpdate(function () {
     for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
-        if (alienEnemy.x < 0) {
+        if (value.x < 6) {
+            info.changeLifeBy(-1)
             info.changeScoreBy(-2)
+            sprites.destroy(value)
         }
     }
 })
@@ -352,58 +339,49 @@ game.onUpdateInterval(1000, function () {
         . . . 2 . . . 
         `,
     img`
-        4 4 4 . . 4 4 4 4 4 . . . . . . 
-        4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
-        b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
-        . b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
-        . b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
-        b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
-        c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
-        c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
-        c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
-        c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
-        . c 4 5 5 5 5 d d d 5 5 5 5 5 b 
-        . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
-        . . c 4 4 d 4 4 4 4 4 d d 5 d c 
-        . . . c 4 4 4 4 4 4 4 4 5 5 5 4 
-        . . . . c c b 4 4 4 b b 4 5 4 4 
-        . . . . . . c c c c c c b b 4 . 
+        . . . 7 . . . . . . 
+        . . . . . . . . 7 . 
+        . . . 1 1 1 1 1 . . 
+        6 . . 1 . . . 1 . . 
+        . . . 1 1 1 1 1 . . 
+        . . . . 1 . 1 . . 6 
+        . . . . 1 . 1 . . . 
+        . . 6 . 1 . 1 . . . 
+        . . . . 1 . 1 . 7 . 
+        . . . 1 . . . 1 . . 
+        . . 1 . . . . 7 1 . 
+        . 1 . 7 7 . 6 7 7 1 
+        . 1 7 6 7 7 7 1 7 1 
+        . 1 7 7 7 7 1 7 7 1 
+        . . 1 7 7 6 7 7 1 . 
+        . . . 1 1 1 1 1 . . 
         `,
     img`
-        . . . . c c c b b b b b . . . . 
-        . . c c b 4 4 4 4 4 4 b b b . . 
-        . c c 4 4 4 4 4 5 4 4 4 4 b c . 
-        . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-        e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-        e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-        e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-        . e b 4 4 4 4 4 5 4 4 4 4 b e . 
-        8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-        8 7 2 e e e e e e e e e e 2 7 8 
-        e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-        e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-        e b e 8 8 c c 8 8 c c c 8 e b e 
-        e e b e c c e e e e e c e b e e 
-        . e e b b 4 4 4 4 4 4 4 4 e e . 
-        . . . c c c c c e e e e e . . . 
+        . . 8 8 8 8 8 8 . . 
+        . 8 8 8 3 8 8 3 8 . 
+        8 8 3 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 3 8 8 
+        . a a a a a a a a . 
+        7 7 7 7 7 7 7 7 7 7 
+        . 5 5 5 5 5 5 5 5 . 
+        8 8 3 8 8 5 5 5 8 8 
+        . 8 8 3 8 8 8 3 8 . 
         `,
     img`
-        . . 2 2 b b b b b . . . . . . . 
-        . 2 b 4 4 4 4 4 4 b . . . . . . 
-        2 2 4 4 4 4 d d 4 4 b . . . . . 
-        2 b 4 4 4 4 4 4 d 4 b . . . . . 
-        2 b 4 4 4 4 4 4 4 d 4 b . . . . 
-        2 b 4 4 4 4 4 4 4 4 4 b . . . . 
-        2 b 4 4 4 4 4 4 4 4 4 e . . . . 
-        2 2 b 4 4 4 4 4 4 4 b e . . . . 
-        . 2 b b b 4 4 4 b b b e . . . . 
-        . . e b b b b b b b e e . . . . 
-        . . . e e b 4 4 b e e e b . . . 
-        . . . . . e e e e e e b d b b . 
-        . . . . . . . . . . . b 1 1 1 b 
-        . . . . . . . . . . . c 1 d d b 
-        . . . . . . . . . . . c 1 b c . 
-        . . . . . . . . . . . . c c . . 
+        . . c c c c c c c c . . 
+        . c c d c c c d c c c . 
+        . c c c c d c c c c c . 
+        c c c c c c c c c d c c 
+        c c d c c c d c c c c c 
+        . c c c c c c c c c c . 
+        . . 3 c c 3 3 3 3 8 . . 
+        . . 3 3 c 3 3 8 3 8 . . 
+        . . . 3 c 3 8 3 8 . . . 
+        . . . 3 3 3 3 3 8 . . . 
+        . . . . 3 3 3 8 . . . . 
+        . . . . 3 3 3 8 . . . . 
+        . . . . . 3 8 . . . . . 
+        . . . . . 3 8 . . . . . 
         `
     ]
     EnemyDifficultySpawner(gameModeDifficulty)
